f41c0847ca57f332470b73008dc96ed8dd7c806d
youmemonk <ukeym160@gmail.com>
2022-01-06 23:42:43 +0530

    post 1.0

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,5 @@
 import CreateIcon from "@material-ui/icons/Create";
-import React from "react";
+import React, { useState } from "react";
 import "./Feed.css";
 import InputOption from "./InputOption";
 
@@ -7,8 +7,22 @@ import ImageIcon from "@material-ui/icons/Image";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from "./Post";
+
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+
+    setPosts([...posts])
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -17,7 +31,9 @@ function Feed() {
 
           <form>
             <input type="text" />
-            <button type="submit">Send</button>
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
 
@@ -32,9 +48,28 @@ function Feed() {
             color="#7fc15e"
           />
         </div>
-      </div>
 
-      {/* Posts */}
+        {/* Posts */}
+        {posts.map((post) => (
+          <Post />
+        ))}
+        <Post
+          name="Mayank Ukey"
+          description="This is a test"
+          message="this message is working"
+        />
+
+        <div className="post__buttons">
+          <InputOption
+            Icon={ThumbUpAltOutlinedIcon}
+            title="Like"
+            color="gray"
+          />
+          <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+          <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+          <InputOption Icon={SendOutlinedIcon} title="Send" color="gray" />
+        </div>
+      </div>
     </div>
   );
 }
